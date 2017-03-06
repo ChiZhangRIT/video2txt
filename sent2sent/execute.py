f0d19270b54f91d5d23d7804ccb2fdaac3950f62
ChiZhangRIT <ametee@gmail.com>
2017-03-05 20:10:08 -0500

    update 03032017

--- sent2sent/execute.py
+++ sent2sent/execute.py
@@ -56,18 +56,6 @@ def get_config(config_file='seq2seq.ini'):
 # See seq2seq_model.Seq2SeqModel for details of how they work.
 _buckets = [(5, 10), (10, 15), (20, 25), (40, 50)]
 
-def variable_summaries(var):
-    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
-    with tf.name_scope('summaries'):
-        mean = tf.reduce_mean(var)
-        tf.summary.scalar('mean', mean)
-        with tf.name_scope('stddev'):
-            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-        tf.summary.scalar('stddev', stddev)
-        tf.summary.scalar('max', tf.reduce_max(var))
-        tf.summary.scalar('min', tf.reduce_min(var))
-        tf.summary.histogram('histogram', var)
-
 
 def read_data(source_path, target_path, max_size=None):
   """Read data from source and target files and put into buckets.
